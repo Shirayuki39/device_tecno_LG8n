8425b18b8646bb44bc98a9ee2593a8b887e70a4c
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-10-14 01:44:49 +0700

    LG8n: Initial skeleton tree
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/tecno/LG8n
+
+# Inherit the proprietary files
+include vendor/tecno/LG8n/BoardConfigVendor.mk
---
+++ lineage_LG8n.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/tecno/LG8n/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+BOARD_VENDOR := TECNO
+PRODUCT_NAME := lineage_LG8n
+PRODUCT_DEVICE := LG8n
+PRODUCT_MANUFACTURER := TECNO
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO LG8n
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sys_tssi_64_armv82_tecno-user 12 SP1A.210812.016 336942 release-keys" \
+    PRODUCT_NAME=LG8n-GL
+
+BUILD_FINGERPRINT := TECNO/LG8n-GL/TECNO-LG8n:12/SP1A.210812.016/230526V2421:user/release-keys
